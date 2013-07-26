90f164ea597da4ac44aea2e69f56c9864751da1d
samanhappy <samanhappy@qq.com>
2013-07-26 16:50:58 +0800

    first commit

---
+++ src/main/java/com/coosam/demo/DesignPattern/App.java
@@ -0,0 +1,13 @@
+package com.coosam.demo.DesignPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
